375b72cd89842905cc94009fe035fb464723e624
Nicolas Martinez <124569476+Nico13M@users.noreply.github.com>
2023-09-18 10:14:43 +0200

    Delete Register_Login_Form directory

--- Register_Login_Form/inc/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-setcookie('remember', NULL, -1);
-unset($_SESSION['auth']);
-$_SESSION['flash']['sucess'] ='Vous etes maintenant déconnecté';
-header('Location: login.php');
